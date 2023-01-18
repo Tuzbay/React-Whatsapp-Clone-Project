dc17eec6b2aef2e09c3be6fc7288814f13f3d277
Tuzbay <t.uzbayyelce@gmail.com>
2023-01-18 17:35:45 +0300

    Dropdown Menus added

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,4 @@
-import { Avatar, IconButton } from "@material-ui/core";
+import { Avatar, IconButton, Menu, MenuItem } from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined } from "@mui/icons-material";
 import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
 import MicIcon from "@material-ui/icons/Mic";
@@ -8,6 +8,12 @@ import { useParams } from "react-router-dom";
 import { db } from "./firebase";
 import firebase from "firebase/compat/app";
 import { useStateValue } from "./StateProvider";
+import PersonIcon from "@mui/icons-material/Person";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
+import PhotoFilterIcon from "@mui/icons-material/PhotoFilter";
+import ImageIcon from "@mui/icons-material/Image";
+import PollIcon from "@mui/icons-material/Poll";
 
 function Chat() {
   const [seed, setSeed] = useState("");
@@ -16,6 +22,13 @@ function Chat() {
   const [roomName, setRoomName] = useState("");
   const [messages, setMessages] = useState([]);
   const [{ user }, dispatch] = useStateValue();
+
+  // ! Aşağısı sonradan ekleme ! \\
+
+  const [openDocument, setOpenDocument] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
+
+  // ! Yukarısı sonradan ekleme ! \\
 
   useEffect(() => {
     if (roomId) {
@@ -66,12 +79,21 @@ function Chat() {
           <IconButton>
             <SearchOutlined />
           </IconButton>
+
           <IconButton>
-            <AttachFile />
+            <MoreVert onClick={() => setDropdown(!dropdown)} />
           </IconButton>
-          <IconButton>
-            <MoreVert />
-          </IconButton>
+          {dropdown && (
+            <div className="menu">
+              <ul className="menuList">
+                <li className="menuListItem">Group Info</li>
+                <li className="menuListItem">Select Message</li>
+                <li className="menuListItem">Unmute Notification</li>
+                <li className="menuListItem">Clear Messages</li>
+                <li className="menuListItem">Leave the Group</li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
 
@@ -98,7 +120,37 @@ function Chat() {
       </div>
 
       <div className="chat__footer">
-        <InsertEmoticonIcon />
+        <InsertEmoticonIcon style={{ cursor: "pointer" }} />
+        <AttachFile
+          style={{ transform: "rotate(45deg)", cursor: "pointer" }}
+          onClick={() => setOpenDocument(!openDocument)}
+        />
+        {openDocument && (
+          <div className="documentation">
+            {/* <li className="documentation__element">
+              <PollIcon /> <div className="popup"> Survey</div>
+            </li> */}
+            <li className="documentation__element">
+              <PersonIcon /> <div className="popup"> Contact</div>
+            </li>
+            <li className="documentation__element">
+              <InsertDriveFileIcon />
+              <div className="popup"> Document</div>
+            </li>
+            <li className="documentation__element">
+              <CameraAltIcon />
+              <div className="popup"> Camera</div>
+            </li>
+            <li className="documentation__element">
+              <PhotoFilterIcon />
+              <div className="popup sticker"> Sticker</div>
+            </li>
+            <li className="documentation__element">
+              <ImageIcon />
+              <div className="popup photosvideos"> Photos & Videos</div>
+            </li>
+          </div>
+        )}
         <form>
           <input
             value={input}
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./Sidebar.css";
-import { Avatar, IconButton } from "@material-ui/core";
+import { Avatar, IconButton, Menu, MenuItem } from "@material-ui/core";
 import DonutLargeIcon from "@mui/icons-material/DonutLarge";
 import ChatIcon from "@mui/icons-material/Chat";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import GroupsIcon from "@mui/icons-material/Groups";
+import FilterListIcon from "@mui/icons-material/FilterList";
 import { SearchOutlined } from "@mui/icons-material";
 import SidebarChat from "./SidebarChat";
 import { db } from "./firebase";
@@ -12,6 +14,20 @@ import { useStateValue } from "./StateProvider";
 function Sidebar() {
   const [rooms, setRooms] = useState([]);
   const [{ user }, dispatch] = useStateValue();
+  const [dropdown, setDropdown] = useState(false);
+  // ! Aşağısı sonradan ekleme ! \\
+
+  const createChat = () => {
+    const roomName = prompt("Please Enter Name For Chat Room");
+
+    if (roomName) {
+      db.collection("rooms").add({
+        name: roomName,
+      });
+    }
+  };
+  // ! Yukarısı sonradan ekleme ! \\
+
   useEffect(() => {
     const unsubscribe = db.collection("rooms").onSnapshot((snapshot) =>
       setRooms(
@@ -30,8 +46,11 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar src={user?.photoURL} />
+        <Avatar src={user?.photoURL} className="avatar" />
         <div className="sidebar__headerRight">
+          <IconButton>
+            <GroupsIcon />
+          </IconButton>
           <IconButton>
             <DonutLargeIcon />
           </IconButton>
@@ -39,20 +58,33 @@ function Sidebar() {
             <ChatIcon />
           </IconButton>
           <IconButton>
-            <MoreVertIcon />
+            <MoreVertIcon onClick={() => setDropdown(!dropdown)} />
           </IconButton>
+          {dropdown && (
+            <div className="menu">
+              <ul className="menuList">
+                <li className="menuListItem" onClick={createChat}>
+                  New Group
+                </li>
+                <li className="menuListItem">New Community</li>
+                <li className="menuListItem">Starred Messages</li>
+                <li className="menuListItem">Settings</li>
+                <li className="menuListItem">Log Out</li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
 
       <div className="sidebar__search">
         <div className="sidebar__searchContainer">
-          <SearchOutlined />
+          <SearchOutlined className="search1" />
           <input placeholder="Search or start new chat" type="text" />
         </div>
+        <FilterListIcon style={{ color: "gray" }} />
       </div>
 
       <div className="sidebar__chats">
-        <SidebarChat addNewChat />
         {rooms.map((room) => (
           <SidebarChat key={room.id} id={room.id} name={room.data.name} />
         ))}
